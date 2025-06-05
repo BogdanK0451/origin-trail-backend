bdcf0f5e002832d541b6431188eedbb55953d0ea
Bogdan Korać <bogdan.korac@gdi.net>
2025-06-05 03:17:26 +0200

    chore: vercel config

--- app.js
+++ app.js
@@ -15,19 +15,15 @@ var balanceRouter = require('./routes/balance');
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(cors({
-  origin: process.env.CORSHOST,
-}));
+app.use(cors());
+
+
 
 app.use('/', indexRouter);
 app.use('/transactions', transactionsRouter);
